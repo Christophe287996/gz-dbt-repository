c7709dde5fb7c4e402b42c06c83b5d7b3b273547
alves.pro.christophe@gmail.com <alves.pro.christophe@gmail.com>
2024-10-29 15:31:14 +0000

    ghghgh

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -3,9 +3,9 @@ Select
     , date_date
     , revenue
     , quantity
-    ,ROUND((SUM(purchase_price)*SUM(quantity)),2) AS purchase_cost
-    ,ROUND(((SUM(purchase_price)*SUM(quantity))-revenue),2) AS margin
+    , products_id
+    ,ROUND(purchase_price*quantity,2) AS purchase_cost
+    ,ROUND(revenue-(purchase_price*quantity),2) AS margin
 FROM {{ref("stg_raw__sales")}}
 JOIN {{ref("stg_raw__product")}}
 USING (products_id)
-GROUP BY 1,2,3,4
