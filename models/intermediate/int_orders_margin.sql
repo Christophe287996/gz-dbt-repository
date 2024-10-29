80ebc3dfbae2bbb850af8f9939aad7023d412d5c
alves.pro.christophe@gmail.com <alves.pro.christophe@gmail.com>
2024-10-29 15:25:45 +0000

    dght

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,12 +1,10 @@
 Select
-    int_sales_margin.orders_id
+      orders_id
     , date_date
-    , revenue
-    , quantity
-    , purchase_cost
-    , margin
+    , ROUND(SUM(revenue),2) AS revenue
+    , ROUND(SUM(quantity),2) AS quantity
+    , ROUND(SUM(purchase_cost),2) AS purchase_cost
+    , ROUND(SUM(margin),2) AS margin
 FROM {{ref("int_sales_margin")}}
-JOIN {{ref("stg_raw__sales")}}
-USING (products_id)
-GROUP BY orders_id
+GROUP BY orders_id, date_date
 ORDER BY orders_id DESC
