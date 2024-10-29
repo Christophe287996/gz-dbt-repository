e5fccd0a05effe2bd304f671f3d2e07fc20ab135
alves.pro.christophe@gmail.com <alves.pro.christophe@gmail.com>
2024-10-29 16:36:56 +0000

    d

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,9 @@
+Select
+orders_id
+,date_date
+,quantity
+,revenue
+(margin-ship_cost-shipping_fee-logcost) AS operational_margin
+FROM {{ref("int_orders_margin")}}
+JOIN {{ref("stg_raw__ship")}}
+USING (orders_id)
