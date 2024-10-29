2d4060c9d8c477e39535340549aba1bd64d13738
alves.pro.christophe@gmail.com <alves.pro.christophe@gmail.com>
2024-10-29 15:21:24 +0000

    gh

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,10 +1,12 @@
-Select 
-      purchase_cost
-    , margin
-    , orders_id
+Select
+    int_sales_margin.orders_id
     , date_date
     , revenue
     , quantity
+    , purchase_cost
+    , margin
 FROM {{ref("int_sales_margin")}}
-JOIN {{ref("stg_raw__ship")}}
+JOIN {{ref("stg_raw__sales")}}
 USING (products_id)
+GROUP BY orders_id
+ORDER BY orders_id DESC
--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,7 +1,11 @@
 Select 
+    orders_id
+    , date_date
+    , revenue
+    , quantity
     ,ROUND((SUM(purchase_price)*SUM(quantity)),2) AS purchase_cost
     ,ROUND(((SUM(purchase_price)*SUM(quantity))-revenue),2) AS margin
 FROM {{ref("stg_raw__sales")}}
 JOIN {{ref("stg_raw__product")}}
 USING (products_id)
-GROUP BY revenue
+GROUP BY 1,2,3,4
